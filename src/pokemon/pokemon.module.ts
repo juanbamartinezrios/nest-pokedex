84743a79768febb8e1cc6aa094d1ad48ea626d53
Juanba <jmartinez@mobilecomputing.com.ar>
2023-05-30 12:05:27 -0300

    variables de entorno - validationSchema

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -3,11 +3,13 @@ import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [PokemonController],
   providers: [PokemonService],
   imports: [
+    ConfigModule,
     // the forFeature() method to configure the module, including defining which models should be registered in the current scope.
     MongooseModule.forFeature([
       {
